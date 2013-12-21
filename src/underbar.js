a1e4991f347faf10176fac8145ff672fe0d6225a
Alex Jacobs <lex.jacobs@gmail.com>
2013-12-21 12:33:01 -0800

    Refactored with simple slice. But doesn't work for index 0 !!

--- src/underbar.js
+++ src/underbar.js
@@ -29,7 +29,7 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length-1] : array.slice( (array.length-n < 0 ? 0 : array.length-n)  , array.length);
+    return n === undefined ? array[array.length-1] : array.slice(-n,array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
