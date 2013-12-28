d9e8c65c1a211e0794f080fde98abbad175c7890
Alex Jacobs <lex.jacobs@gmail.com>
2013-12-28 15:19:27 -0800

    ._extend successful, with a helping hand from underscore. Will try to refactor to reinforce understanding.

--- src/underbar.js
+++ src/underbar.js
@@ -310,8 +310,15 @@ var _ = { };
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
+  
   _.extend = function(obj) {
-  };
+    _.each(arguments, function(item){
+      for (var props in item) {
+        obj[props] = item[props];
+      }
+    });
+      return obj;
+    };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
